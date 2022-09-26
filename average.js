5e64289d4b25184704228fb5aea197864304fcfc
Zainul Muhammad Rofa <113880540+zainulrofa@users.noreply.github.com>
2022-09-26 10:32:18 +0700

    average.js

--- average.js
+++ average.js
@@ -1,26 +1,26 @@
-const mtk = 100
-const bind = 80
-const bing = 85
-const ipa = 95
-let rataNilai = (mtk + bind + bing + ipa) / 4
-
-function nilaiAkhir() {
-    if (typeof mtk !== "number" || typeof bind !== "number" || typeof bing !== "number" || typeof ipa !== "number") return "Data Harus Number"
+function nilaiAkhir(mtk, bind, bing, ipa) {
+    if (typeof mtk !== "number" || mtk === undefined ||
+        typeof bind !== "number" || bind === undefined ||
+        typeof bing !== "number" || bing === undefined ||
+        typeof ipa !== "number" || ipa === undefined) {
+        return "Data Harus Number"
+    }
+    let rataNilai = (mtk + bind + bing + ipa) / 4
+    console.log("Rata-Rata: " + rataNilai)
     if (rataNilai >= 90 && rataNilai <= 100) {
         return "Grade: A"
     } else
-    if (rataNilai >= 80 && rataNilai <= 89) {
+    if (rataNilai >= 80 && rataNilai < 90) {
         return "Grade: B"
     } else
-    if (rataNilai >= 70 && rataNilai <= 79) {
+    if (rataNilai >= 70 && rataNilai < 80) {
         return "Grade: C"
     } else
-    if (rataNilai >= 60 && rataNilai <= 69) {
+    if (rataNilai >= 60 && rataNilai < 70) {
         return "Grade: D"
     } else {
         return "Grade E"
     }
 }
 
-console.log("Rata-Rata: " + rataNilai)
-console.log(nilaiAkhir())
+console.log(nilaiAkhir(100, 80, 85, 90))
